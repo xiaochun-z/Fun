4df2807cfa6b0e7e7c113d964b7271470a1433ba
caden <io.oi@live.com>
2017-07-18 14:20:25 +0800

    support http both get & post

--- httpserver/myhttpserver.py
+++ httpserver/myhttpserver.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-#--coding:utf-8--
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from os import path
@@ -8,57 +7,66 @@ from urllib.parse import urlparse
 curdir = path.dirname(path.realpath(__file__))
 sep = '/'
 
-# MIME-TYPE
-mimedic = [
-						('.html', 'text/html'),
-						('.htm', 'text/html'),
-						('.js', 'application/javascript'),
-						('.css', 'text/css'),
-						('.json', 'application/json'),
-						('.png', 'image/png'),
-						('.jpg', 'image/jpeg'),
-						('.gif', 'image/gif'),
-						('.txt', 'text/plain'),
-						('.avi', 'video/x-msvideo'),
-						('.woff', 'application/x-font-woff'),
-					]
 
+# HTTPRequestHandler class
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
-	# GET
-	def do_GET(self):
-		sendReply = False
-		querypath = urlparse(self.path)
-		filepath, query = querypath.path, querypath.query
-		
-		if filepath.endswith('/'):
-			filepath += 'index.html'
-		filename, fileext = path.splitext(filepath)
-		for e in mimedic:
-			if e[0] == fileext:
-				mimetype = e[1]
-				sendReply = True
+    # GET
+    def do_GET(self):
+        self.handle_http_request()
 
-		if sendReply == True:
-			try:
-				with open(path.realpath(curdir + sep + filepath),'rb') as f:
-					content = f.read()
-					self.send_response(200)
-					self.send_header('Content-type',mimetype)
-					self.end_headers()
-					self.wfile.write(content)
-			except IOError:
-				self.send_error(404,'File Not Found: %s' % self.path)
+    def do_POST(self):
+        self.handle_http_request();
+
+    def handle_http_request(self):
+        sendReply = False
+        querypath = urlparse(self.path)
+        filepath, query = querypath.path, querypath.query
+        if filepath.endswith('/'):
+            filepath += 'index.html'
+        if filepath.endswith(".html"):
+            mimetype = 'text/html'
+            sendReply = True
+        if filepath.endswith(".jpg"):
+            mimetype = 'image/jpg'
+            sendReply = True
+        if filepath.endswith(".gif"):
+            mimetype = 'image/gif'
+            sendReply = True
+        if filepath.endswith(".js"):
+            mimetype = 'application/javascript'
+            sendReply = True
+        if filepath.endswith(".css"):
+            mimetype = 'text/css'
+            sendReply = True
+        if filepath.endswith(".json"):
+            mimetype = 'application/json'
+            sendReply = True
+        if filepath.endswith(".woff"):
+            mimetype = 'application/x-font-woff'
+            sendReply = True
+        if sendReply == True:
+            # Open the static file requested and send it
+            try:
+                with open(path.realpath(curdir + sep + filepath), 'rb') as f:
+                    content = f.read()
+                    self.send_response(200)
+                    self.send_header('Content-type', mimetype)
+                    self.end_headers()
+                    self.wfile.write(content)
+            except IOError:
+                self.send_error(404, 'File Not Found: %s' % self.path)
+
 
 def run():
-	port = 8000
-	print('starting server, port', port)
+    port = 8000
+    print('starting server, port', port)
 
-	# Server settings
-	server_address = ('', port)
-	httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
-	print('running server...')
-	httpd.serve_forever()
+    # Server settings
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+    print('running server...')
+    httpd.serve_forever()
 
 
 if __name__ == '__main__':
-	run()
+    run()

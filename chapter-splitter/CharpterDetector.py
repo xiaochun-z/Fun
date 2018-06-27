10ebf6fa5d59e716187d1f28a022c25c1cffcfd7
CORP\xiaocz <Zhou.XiaoChun@emc.com>
2018-06-27 19:40:16 +0800

    support more format and add more comments, use html tag instead of <pre>

--- chapter-splitter/CharpterDetector.py
+++ chapter-splitter/CharpterDetector.py
@@ -1,4 +1,3 @@
-from collections import deque
 import re
 import os
 import errno
@@ -8,7 +7,9 @@ class ChapterMaster:
     def __init__(self, fpath: str):
         self.__filename = fpath
         self.__path = os.path.dirname(os.path.abspath(fpath))
-        self.keywords = "^\s*[第地](\d+)章.+$"
+        # 章节的正则表达式，多数小说是“第1002章 咒怨”这类格式，如果小说不是这样的格式，同时实现多种正则表达式
+        self.keywords = ["^\s*[第地](\d+)章.+$", "^\s*[第地]([一二三四五六七八九十百千万]+)章.+$"]
+
         self.__navPointTemplate = """
     <navPoint id="navPoint-{0}" playOrder="{0}">
         <navLabel>
@@ -18,16 +19,14 @@ class ChapterMaster:
     </navPoint>
         """
 
-        self.__xhtmlTemplate = """
- <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+        self.__xhtmlTemplate = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
 <title>{0}</title>
 </head>
 <body>
-<pre>
 {1}
-</pre>
 </body>
 </html>
         """
@@ -35,9 +34,9 @@ class ChapterMaster:
     def generateTOC(self, chapter: str, title: str) -> (str, str):
         """
         generate toc based on the template
-        :param chapter:
-        :param title:
-        :return:
+        :param chapter: the chapter count
+        :param title: the chapter title
+        :return: filename + navPoint
         """
         c = chapter.zfill(5)
         filename = str.format("Section{0}", c)
@@ -46,31 +45,42 @@ class ChapterMaster:
     def parse(self) -> list:
         """
         parse the file with some pre-defined keywords.
-        :rtype: deque
+        :rtype: list
         """
         print("parse file name " + self.__filename)
         lst = list()
-        regex = re.compile(self.keywords, re.IGNORECASE)
+        regexps = [re.compile(keyword, re.IGNORECASE) for keyword in self.keywords]
         try:
             with(open(self.__filename, 'r', encoding="utf-8")) as f:
-                cnt = 1
+                # 原本的小说的行数（第1行，第2行）
+                cnt: int = 1
+                # 章节数（第5章，第6章之类的）
+                chaptercnt: int = 0
                 while True:
                     line = f.readline()
                     if not line:
                         break
-                    if regex.match(line):
-                        search = regex.search(line)
-                        print(str.format("行数:{0}, 章节序号:{1}, 名称:{2}", cnt, search.group(1), line))
-                        lst.append((cnt, search.group(1), line))
+                    for regex in regexps:
+                        if regex.match(line):
+                            search = regex.search(line)
+                            chaptercnt += 1
+                            print(str.format("行数:{0}, 计算序号:{1}, 书中章节序号:{2}, 章节名称:{3}", cnt, chaptercnt, search.group(1),
+                                             line))
+                            lst.append((cnt, chaptercnt, search.group(1), line))
                     cnt += 1
         except Exception as e:
             print(str(e))
         finally:
             print("end process")
-            f.close()
         return lst
 
-    def preparseTextDirectory(self, path: str):
+    @staticmethod
+    def prepareTextDirectory(path: str):
+        """
+        create the directory if this directory does not exist
+        :param path:
+        :return:
+        """
         try:
             os.makedirs(path)
         except OSError as e:
@@ -78,14 +88,21 @@ class ChapterMaster:
                 raise
 
     def split(self, it: list):
+        """
+        split the novel into different chapters.
+        :param it: a list of chapters
+        :return:
+        """
         textpath = os.path.join(self.__path, "Text")
-        self.preparseTextDirectory(textpath)
+        self.prepareTextDirectory(textpath)
         toc = ''
         cnt = 1
         for i in range(0, len(it) - 1):
             chapter = it[i]
             nxt = it[i + 1]
-            t = self.generateTOC(chapter[1], chapter[2])
+            chapternum = str(chapter[1])
+            chaptertitle = chapter[3]
+            t = self.generateTOC(chapternum, chaptertitle)
             toc += t[1]
             content = self.getChapterContent(chapter[0] - 1, nxt[0] - 1, chapter[2])
             fn = os.path.join(self.__path, "Text\\" + t[0] + ".xhtml")
@@ -94,13 +111,33 @@ class ChapterMaster:
                 f.write(content)
             cnt += 1
 
+            if cnt > 1000:
+                break
+
         tocfn = os.path.join(self.__path, "toc.txt")
         with(open(tocfn, "w", encoding="utf-8")) as f:
             f.write(toc)
 
     def getChapterContent(self, line1: int, line2: int, title) -> str:
         with(open(self.__filename, 'r', encoding='utf-8')) as f:
-            return str.format(self.__xhtmlTemplate, title, "".join(f.readlines()[line1:line2]))
+            lines = f.readlines()
+            title = lines[line1].rstrip()
+            body = "".join(lines[line1 + 1:line2])
+            body = str.format("<h1>{0}</h1>\n", title) + self.convertToHTML(body)
+            return str.format(self.__xhtmlTemplate, title, body)
+
+    @staticmethod
+    def convertToHTML(text: str) -> str:
+        formatted = text.replace('\n', '</p><p>')
+        formatted = formatted.replace("</p><p>", "<p>", 1)
+        formatted = ChapterMaster.rreplace(formatted, "</p><p>", "</p>", 1)
+        formatted = formatted.replace("<p></p>", "")
+        return formatted.replace(" ", "")
+
+    @staticmethod
+    def rreplace(s, old, new, occurrence):
+        li = s.rsplit(old, occurrence)
+        return new.join(li)
 
     @staticmethod
     def checking(result):
@@ -114,8 +151,10 @@ class ChapterMaster:
             nxt = next(iterator, None)
             if not nxt:
                 break
-            c = int(chapter[1])
-            n = int(nxt[1])
+            if chapter[2] is not int:
+                break
+            c = int(chapter[2])
+            n = int(nxt[2])
             if n != c + 1:
                 print(str.format("没有找到章节 {0} , 应该在 {1} 行以后", c + 1, chapter[0]))
 
